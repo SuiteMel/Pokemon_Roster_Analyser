8090ebe737423c9706b8825aa6512f5d84f4a620
Melissa Waltner <melissamwaltner@gmail.com>
2018-03-07 01:14:06 -0600

    Created card item dynamically with jquery

--- assets/javascript/pokeApp.js
+++ assets/javascript/pokeApp.js
@@ -1,23 +1,25 @@
 
-var query = "marshtomp";
+var query = "trapinch";
 var queryURL = "https://pokeapi.co/api/v2/pokemon/" + query;
-// queryURL += '?' + $.param({
-  
-// });
-//console.log(queryURL);
+
 $.ajax({
   url: queryURL,
   method: 'GET'
 }).then(function (response) {
   console.log(queryURL);
-  var pokeName = $("<p>").text(response.name);
-    
-  var pokeImage = response.sprites.front_default;
 
-  var imageRend = $("<img>").attr("src", pokeImage);
+  cardDiv = $("<div>").addClass("card text-center w-50 m-3 text-capitalize");
+  cardHead = $("<div>").addClass("card-header");
+  cardBody = $("<div>").addClass("card-body");
+
+  var pokeName = $("<span>").text(response.name).addClass("h3");   
+  var pokeImage = response.sprites.front_default;
+  var imageRend = $("<img>").attr("src", pokeImage).addClass("rounded-circle bg-secondary");
+  var pokeLabel = cardHead.append(imageRend, pokeName);
 
   var abil = $("<ul>").text("Abilities: ").addClass("list-inline");
   var types = $("<ul>").text("Type: ").addClass("list-inline");
+  var stats = $("<ul>").text("Stats: ").addClass("list-inline");
 
   for (i = 0; i < response.abilities.length; i++) {
     var abilitys = response.abilities[i].ability.name;
@@ -29,10 +31,17 @@ $.ajax({
     var typeName = $("<li>").text(response.types[j].type.name).addClass("list-inline-item");
     types.append(typeName);
   }
-  
-  
-  (abilityName, typeName);
 
-  $("#pokeReturn").append(pokeName, imageRend, abil, types);
-  $("#pokeReturn").addClass("text-capitalize");
+  for (k=0; k < response.stats.length; k++) {
+    var statName = response.stats[k].stat.name;
+    var statNum = response.stats[k].base_stat;
+
+    var statLabel = $("<li>").text(statName + ": " + statNum).addClass("list-inline-item");
+    stats.append(statLabel);
+  }
+
+  cardBody.append(abil, types, stats);
+  cardDiv.append(cardHead, cardBody);
+
+  $(".container").append(cardDiv);
 });
