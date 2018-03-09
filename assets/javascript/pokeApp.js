fb26a1829b7280977ded46955592c2b2b99f602f
Melissa Waltner <melissamwaltner@gmail.com>
2018-03-08 19:58:06 -0600

    Created dynamic cards and changed the index.html layout

--- assets/javascript/pokeApp.js
+++ assets/javascript/pokeApp.js
@@ -1,54 +1,45 @@
-var party = ["trapinch", "55", "205"];
+var party = ["trapinch", "55", "66", "107", "togepi", "5"];
 
 $.each(party, function (x, y) {
   var URL = "https://pokeapi.co/api/v2/pokemon/" + y;
-  renderCard(URL);
+  renderCard(URL, x);
 });
 
+function renderCard(queryURL, count) {
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function (response) {
 
-function renderCard(queryURL) {
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-}).then(function (response) {
-  console.log(queryURL);
+    var pokeName = $("<span>").text(response.name).addClass("h3");
+    var pokeImage = response.sprites.front_default;
+    var imageRend = $("<img>").attr("src", pokeImage).addClass("rounded-circle bg-secondary mr-1");
+    
+    $("#poke" + count).children(".card-header").append(imageRend, pokeName);
 
-  cardDiv = $("<div>").addClass("card text-center w-50 m-3 text-capitalize");
-  cardHead = $("<div>").addClass("card-header");
-  cardBody = $("<div>").addClass("card-body");
+    var abil = $("<ul>").text("Abilities: ").addClass("list-inline");
+    var types = $("<ul>").text("Type: ").addClass("list-inline");
+    var stats = $("<ul>").text("Stats: ").addClass("list-inline");
 
-  var pokeName = $("<span>").text(response.name).addClass("h3");   
-  var pokeImage = response.sprites.front_default;
-  var imageRend = $("<img>").attr("src", pokeImage).addClass("rounded-circle bg-secondary mr-1");
-  var pokeLabel = cardHead.append(imageRend, pokeName);
+    for (i = 0; i < response.abilities.length; i++) {
+      var abilitys = response.abilities[i].ability.name;
+      var abilityName = $("<li>").text(abilitys).addClass("list-inline-item");
+      abil.append(abilityName);
+    }
 
-  var abil = $("<ul>").text("Abilities: ").addClass("list-inline");
-  var types = $("<ul>").text("Type: ").addClass("list-inline");
-  var stats = $("<ul>").text("Stats: ").addClass("list-inline");
+    for (j = 0; j < response.types.length; j++) {
+      var typeName = $("<li>").text(response.types[j].type.name).addClass("list-inline-item");
+      types.append(typeName);
+    }
 
-  for (i = 0; i < response.abilities.length; i++) {
-    var abilitys = response.abilities[i].ability.name;
-    var abilityName = $("<li>").text(abilitys).addClass("list-inline-item");
-    abil.append(abilityName);
-  }
+    for (k = 0; k < response.stats.length; k++) {
+      var statName = response.stats[k].stat.name;
+      var statNum = response.stats[k].base_stat;
 
-  for (j = 0; j < response.types.length; j++) {
-    var typeName = $("<li>").text(response.types[j].type.name).addClass("list-inline-item");
-    types.append(typeName);
-  }
+      var statLabel = $("<li>").text(statName + ": " + statNum).addClass("list-inline-item");
+      stats.append(statLabel);
+    }
 
-  for (k=0; k < response.stats.length; k++) {
-    var statName = response.stats[k].stat.name;
-    var statNum = response.stats[k].base_stat;
-
-    var statLabel = $("<li>").text(statName + ": " + statNum).addClass("list-inline-item");
-    stats.append(statLabel);
-  }
-
-  cardBody.append(abil, types, stats);
-  cardDiv.append(cardHead, cardBody);
-
-  $("#test").append(cardDiv);
-});
-
+    $("#poke" + count).children(".card-body").append(abil, types, stats);
+  });
 }
